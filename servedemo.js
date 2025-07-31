6e70d32d9614adcfbc55a7d0ba102515690667d9
Chiruvolu18 <saitejaswichiruvolu18@gmail.com>
2025-07-31 15:34:34 -0400

    Delete servedemo.js

--- servedemo.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send("Hello from Node.js Express server!"));
-app.listen(3000, () => console.log("Node server running on port 3000"));
-
-app.get('/greet', (req, res) => res.send("Hello Teju!"));
